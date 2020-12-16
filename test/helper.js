4d6026e1c5ad73da6bf83bacb12572ff7a7ba33f
capricorncd <1355952333@qq.com>
2020-12-16 14:47:21 +0900

    Remove toString(), add obj2str package

--- test/helper.js
+++ test/helper.js
@@ -3,7 +3,7 @@
  * https://github.com/capricorncd
  * Date: 2020-08-11 11:11
  */
-// import ZxCalendar from '../src'
+import { obj2str } from 'obj2str'
 
 function $(s, context = document) {
   return context.querySelector(s)
@@ -49,55 +49,6 @@ function fmtCode(html) {
   })
 }
 
-function arrToString(array, space) {
-  const arr = ['[']
-  array.forEach(item => {
-    if (Array.isArray(item)) {
-      arr.push(arrToString(item))
-    } else if (typeof item === 'function') {
-      arr.push(getSpace(space) + item.toString())
-    } else if (item && typeof item === 'object') {
-      arr.push(toString(item, space + 2))
-    } else {
-      if (typeof item === 'string') {
-        item = `'${item}'`
-      }
-      arr.push(getSpace(space) + item + ',')
-    }
-  })
-  arr.push(getSpace(space - 2) + '],')
-  return arr.join('\n')
-}
-
-function toString(obj, space = 2, noStartSpace = false) {
-  const prefix = noStartSpace ? '' : getSpace(space - 2)
-  const arr = [prefix + '{']
-  let tempValue, typeofValue
-  Object.keys(obj).forEach(key => {
-    tempValue = obj[key]
-    if (Array.isArray(tempValue)) {
-      arr.push(getSpace(space) + key + ': ' + arrToString(tempValue, space + 2))
-    } else if (tempValue && typeof tempValue === 'object') {
-      arr.push(getSpace(space) + key + ': ' + toString(tempValue, space + 2, true))
-    } else {
-      typeofValue = typeof tempValue
-      if (typeofValue === 'function') {
-        tempValue = tempValue.toString()
-      } else if (typeofValue === 'string') {
-        tempValue = `'${tempValue}'`
-      }
-      arr.push(getSpace(space) + key + ': ' + tempValue + ',')
-    }
-  })
-  arr.push(getSpace(space - 2) + '}')
-  return arr.join('\n')
-}
-
-const SPACE = ' '
-function getSpace(count = 2) {
-  return new Array(count).fill(SPACE).join('')
-}
-
 function createCalendar(options, title, el) {
   el = el || $('.container')
   const wrapper = createEl('dl', {})
@@ -112,7 +63,7 @@ function createCalendar(options, title, el) {
     el: calendarWrapper
   })
   const pre = createEl('pre', {
-    html: toString(options)
+    html: obj2str(options, { indentSpaces: 2 })
   }, true)
   dd.appendChild(pre)
   wrapper.appendChild(dd)
@@ -143,6 +94,5 @@ export {
   createCalendar,
   createEl,
   log,
-  logStr,
-  toString
+  logStr
 }
